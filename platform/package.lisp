778857956d6a4c91796926e111180de86b5a2e67
cyclohexanamine <cyclohexanamine@users.noreply.github.com>
2016-09-28 01:31:30 +0100

    Removing redundant imports.

--- platform/package.lisp
+++ platform/package.lisp
@@ -4,8 +4,7 @@
 
 (defpackage :thread
   (:use :cl :mac)
-  (:export :make-thread :current-thread :threadp :thread-name :newthread
-           :make-recursive-lock :acquire-recursive-lock :release-recursive-lock :with-recursive-lock-held
+  (:export :newthread
            :threaded-object :defslotinterface :defslotints :defclass-threaded :defprocessors :with-slot :with-slot-lock :modify-slot
            :push-queue :pop-queue :read-queue :make-processors :start-processors))
 
--- platform/thread/backend/thread-bordeaux.lisp
+++ platform/thread/backend/thread-bordeaux.lisp
@@ -2,11 +2,7 @@
 
 (in-package :thread)
 
-(shadowing-import '(bordeaux-threads:make-thread bordeaux-threads:current-thread bordeaux-threads:threadp bordeaux-threads:thread-name
-                    bordeaux-threads:make-recursive-lock bordeaux-threads:acquire-recursive-lock bordeaux-threads:release-recursive-lock bordeaux-threads:with-recursive-lock-held))
-                    
-(export '(make-thread current-thread threadp thread-name
-          make-recursive-lock acquire-recursive-lock release-recursive-lock with-recursive-lock-held))
+(shadowing-import '(bordeaux-threads:make-thread bordeaux-threads:make-recursive-lock bordeaux-threads:with-recursive-lock-held))
           
 (defmacro newthread (fname &rest args)
   `(make-thread
