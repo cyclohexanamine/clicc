34bf5570fad0e04d6c9712cc9189cbe036d0f023
cyclohexanamine <cyclohexanamine@users.noreply.github.com>
2016-09-28 18:34:28 +0100

    Separated new connection listener from open connections.

--- platform/connection/tcp-usocket/tcp-connection-manager.lisp
+++ platform/connection/tcp-usocket/tcp-connection-manager.lisp
@@ -16,28 +16,27 @@
 ;;; Internal processors
 
 ;; This loop listens for activity on open connections, and prunes dead connections.
-(defun listener-loop (manager)
+(defun connections-loop (manager)
   ;; In the loop, wait for connection activity.
   (let ((conns (wait-for-tcp-connections manager)))
     (loop for conn in conns do
-      (cond ((read-listener-p conn) ; This is a listener, so accept a new connection.
-              (accept-connection manager conn))
-            ((not (is-alive conn)) ; This connection has died.
-              (remove-connection manager conn))
-            (T ; Otherwise, this is a normal connection, so check for a message.
-              (process-message manager conn))))
+      ;; Read messages from active connections.
+      (if (is-alive conn) (process-message manager conn)))
     ;; Also, prune dead connections in general.
     (thread:with-slot manager connections
       (loop for conn in connections do
-        (if (and (not (read-listener-p conn)) (not (is-alive conn)))
+        (if (not (is-alive conn))
             (remove-connection manager conn))))))
 
-;; Start the listener and add it to the main connection list.
+;; Start the listener and put it into the 'listener slot.
 (defun listener-init (manager)
   (setf (read-listener manager)
     (let ((addr (slot-value manager 'address)))
-      (make-tcp-listener (usocket:socket-listen (car addr) (cadr addr)))))
-  (add-connection manager (read-listener manager)))
+      (make-tcp-listener (usocket:socket-listen (car addr) (cadr addr))))))
+      
+;; This loop listens for incoming connections and adds them as they come.
+(defun listener-loop (manager)
+  (accept-connection manager (read-listener manager)))
 
 ;; Here we handle messages in the main message queue:
 ;;  send message, receive message, open connection.
@@ -49,11 +48,15 @@
 
 ;; Define the internal processors, using the functions above.
 (thread:defprocessors tcp-connection-manager
-  ;; Connection listener
+  ;; New connection listener
   (listener (:loop-func #'listener-loop
-              :queue NIL ; Don't receive messages, since we want to always be listening to connections.
+              :queue NIL
               :threads 1)
     (:init-func #'listener-init))
+  ;; Open connections listener
+  (connections (:loop-func #'connections-loop
+              :queue NIL
+              :threads 2))
   ;; Main queue processor
   (main (:loop-func #'queue-loop
           :queue T ; Receive messages from the 'main queue.
@@ -74,12 +77,14 @@
 
 ;; This wraps usocket:wait-for-input, allowing the same functionality but for connection
 ;; objects instead of sockets. It's safe to modify the connection list while another thread
-;; is waiting on connections, but two shouldn't be at once (hence the 'wait' lock).
+;; is waiting on connections, but two shouldn't be at once (hence the 'wait' lock). Given
+;; that the connection list can be modified while waiting, it makes sense to not wait very
+;; long to timeout since any new connections wouldn't be returned here.
 (defmethod-g wait-for-tcp-connections ((manager tcp-connection-manager))
   (thread:with-slot-lock manager wait
     (let* ((socks (thread:with-slot manager (conns 'connections)
                     (mapcar #'read-socket conns)))
-           (input-socks (usocket:wait-for-input socks :timeout 10 :ready-only T)))
+           (input-socks (usocket:wait-for-input socks :timeout 0.05 :ready-only T)))
       (thread:with-slot manager (conns 'connections)
         (remove-if-not
           (lambda (conn) (is-sock-connection-p conn input-socks))
