07e71d052299aafe2423e711f6b814ee514803da
cyclohexanamine <cyclohexanamine@users.noreply.github.com>
2016-09-29 15:25:52 +0100

    Added command line options and REPL.

--- platform/compile-sbcl.lisp
+++ platform/compile-sbcl.lisp
@@ -1,9 +1,11 @@
 (load "platform.asd")
 (asdf:load-system :platform)
+(asdf:load-system :unix-options)
+(use-package :unix-options)
 
 (defun echo-handler (message data manager)
   (connection:send-message-to manager (format NIL "Echoing: ~a" message) (list :id (getf data :id))))
-  
+
 (mac:defnullf nil-handler (message data manager))
 
 (defun print-handler (message data manager) (declare (ignore data manager)) (write-line message))
@@ -11,25 +13,34 @@
 (defun start-manager ()
   (defvar *cm* (make-instance 'connection:tcp-connection-manager :address '(#(0 0 0 0) 5678) :handler #'echo-handler))
   (thread:start-processors *cm*))
-  
+
 (defun stop-manager ()
-  (thread:stop-processors *cm*)
-  (write-line "Stopped processors")
-  (sleep 1)
-  (write-line (format NIL "Processors: ~a" (slot-value *cm* 'thread::processors))))
-    
+  (thread:stop-processors *cm*))
+
+
+
+(defun run-repl ()
+  (format t "~&CLICC REPL~%Compiled with ~a ~a~%"
+          (lisp-implementation-type)
+          (lisp-implementation-version))
+  (sb-thread:with-new-session ()
+    (sb-impl::toplevel-repl nil)))
 
 (defun main ()
-  (restart-case
-      (progn
-        (start-manager)
-        (loop (sleep 0.1)))
-    (abort () 
-      (sb-ext:exit :abort T))
-    (exit () 
-      (progn
-        (stop-manager)
-        (sb-ext:exit :abort NIL :timeout 20)))))
-    
+  (with-cli-options ()
+      (repl &parameters config)
+    (restart-case
+        (progn
+          (start-manager)
+          (if repl
+            (run-repl)
+            (loop (sleep 0.1))))
+      (abort ()
+        (sb-ext:exit :abort T))
+      (exit ()
+        (progn
+          (stop-manager)
+          (sb-ext:exit :abort NIL :timeout 20))))))
+
 
 (sb-ext:save-lisp-and-die "platform.exe" :toplevel #'main :executable T)
